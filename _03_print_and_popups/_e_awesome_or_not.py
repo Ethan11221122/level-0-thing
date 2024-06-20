85d04547c58777c2c1e6a05cd95eebf3fa596b60
league <yayalisa91@gmail.com>
2024-06-20 11:09:34 -0700

    June120Changes

--- _03_print_and_popups/_e_awesome_or_not.py
+++ _03_print_and_popups/_e_awesome_or_not.py
@@ -14,23 +14,23 @@ if __name__ == '__main__':
     # 2. Print your variable to the console
     print(random)
     # 3. Get the user to enter something that they think is awesome
-    question = simpledialog.askstring(title='question', prompt="enter something you think is awesome")
+    simpledialog.askstring(title='question', prompt="enter something you think is awesome")
     # 4. If your variable is  0
         # -- tell the user whatever they entered is awesome!
     if (random==0):
-       anwser0 = simpledialog.askstring(title='awesome', prompt="What you entered is awesome!")
+        messagebox.showinfo(message="What you entered is awesome")
 
     # 5. If your variable is  1
         # -- tell the user whatever they entered is ok.
     if (random==1):
-        anwser1 = simpledialog.askstring(title='Aok', prompt="What you entered is OK")
+        messagebox.showinfo(message="What you entered is ok")
     # 6. If your variable is  2
         # -- tell the user whatever they entered is boring.
     if (random==2):
-        anwser2  = simpledialog.askstring(title='boring', prompt="boring")
+        messagebox.showinfo(message="What you entered is boring")
     # 7. If your variable is  3
         # -- invent your own message to give to the user (be nice).
     if (random==3):
-        anwser3 = simpledialog.askstring(title='Horrible', prompt="way too horrible bru")
+        messagebox.showinfo(message="what you entered is absolutely terrible")
     # Run the window's .mainloop() method
         window.mainloop()
